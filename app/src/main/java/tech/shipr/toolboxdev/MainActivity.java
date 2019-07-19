97163ba3d8d09a5ebc39996db182914ee5702596
yash <yhdesai99@ymail.com>
2019-07-19 06:59:03 +0530

    backing up

--- app/src/main/java/tech/shipr/toolboxdev/MainActivity.java
+++ app/src/main/java/tech/shipr/toolboxdev/MainActivity.java
@@ -1,16 +1,78 @@
 package tech.shipr.toolboxdev;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    final HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, ToolActivity.class));
+        // startActivity(new Intent(MainActivity.this, ToolActivity.class));
 
+
+
+        List<String> cricket = new ArrayList<String>();
+        cricket.add("India");
+        cricket.add("Pakistan");
+        cricket.add("Australia");
+        cricket.add("England");
+        cricket.add("South Africa");
+
+        List<String> football = new ArrayList<String>();
+        football.add("Brazil");
+        football.add("Spain");
+        football.add("Germany");
+        football.add("Netherlands");
+        football.add("Italy");
+
+
+        expandableListDetail.put("CRICKET TEAMS", cricket);
+        expandableListDetail.put("FOOTBALL TEAMS", football);
+
+        expandableListView =  findViewById(R.id.allCategoriesAddedByJava);
+        expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
+        expandableListAdapter = new CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(), expandableListTitle.get(groupPosition) + " List Expanded.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(), expandableListTitle.get(groupPosition) + " List Collapsed.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(getApplicationContext(), expandableListTitle.get(groupPosition) + " -> " + expandableListDetail.get(expandableListTitle.get(groupPosition)).get(childPosition), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
+
+
+
 }
