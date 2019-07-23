7ae2473dc3b512e24034445012672479c3b5a622
yash <yhdesai99@ymail.com>
2019-07-23 11:41:39 +0530

    :construction: working on add tool

---
+++ app/src/main/java/tech/shipr/toolboxdev/view/AddToolAdmin.java
@@ -0,0 +1,14 @@
+package tech.shipr.toolboxdev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddToolAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tool_admin);
+    }
+}
